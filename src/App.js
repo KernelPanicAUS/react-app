4ccd4d686f8fce79d37ad72a8c4856b739675867
Thomas Khalil <thomas@khalilt.com>
2019-03-08 16:19:53 +0100

    prettier

--- src/App.js
+++ src/App.js
@@ -4,50 +4,54 @@ import React, { Component } from 'react';
 import { Dropdown, DropdownItem, DropdownMenu, DropdownToggle, Nav, NavItem, NavLink } from 'reactstrap';
 
 class App extends Component {
-constructor(props) {
-  super(props);
-  this.toggle = this.toggle.bind(this);
-  this.state = {
-    dropdownOpen: false
+  constructor(props) {
+    super(props);
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      dropdownOpen: false
+    };
   }
-}
 
-toggle () {
-  this.setState({
-    dropdownOpen: !this.state.dropdownOpen
-  });
-}
-render() {
-  return (
-    <div>
-      <Nav tabs>
-        <NavItem>
-          <NavLink href="#" active>Link</NavLink>
-        </NavItem>
-        <Dropdown nav isOpen={this.state.dropdownOpen} toggle={this.toggle}>
-          <DropdownToggle nav caret>
-            Dropdown
-          </DropdownToggle>
-          <DropdownMenu>
-            <DropdownItem header>Header</DropdownItem>
-            <DropdownItem disabled>Action</DropdownItem>
-            <DropdownItem>Another Action</DropdownItem>
-            <DropdownItem divider />
-            <DropdownItem>Another Action</DropdownItem>
-          </DropdownMenu>
-        </Dropdown>
-        <NavItem>
-          <NavLink href="#">Link</NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink href="#">Another Link</NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink disabled href="#">Disabled Link</NavLink>
-        </NavItem>
-      </Nav>
-    </div>
-  );
+  toggle() {
+    this.setState({
+      dropdownOpen: !this.state.dropdownOpen
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Nav tabs>
+          <NavItem>
+            <NavLink href="#" active>
+              Link
+            </NavLink>
+          </NavItem>
+          <Dropdown nav isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+            <DropdownToggle nav caret>
+              Dropdown
+            </DropdownToggle>
+            <DropdownMenu>
+              <DropdownItem header>Header</DropdownItem>
+              <DropdownItem disabled>Action</DropdownItem>
+              <DropdownItem>Another Action</DropdownItem>
+              <DropdownItem divider />
+              <DropdownItem>Another Action</DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+          <NavItem>
+            <NavLink href="#">Link</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="#">Another Link</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink disabled href="#">
+              Disabled Link
+            </NavLink>
+          </NavItem>
+        </Nav>
+      </div>
+    );
   }
 }
 
